a656a55392abdc3910c75943d04c99295523bd7c
Diefex <diefex57@gmail.com>
2021-10-06 19:43:08 -0500

    Transferencias

--- contracts/Udicoin.sol
+++ contracts/Udicoin.sol
@@ -3,14 +3,26 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Udicoin {
 	struct Cuenta {
-    string nombre;
+    string contra;
     uint saldo;
   }
-  mapping(uint => Cuenta) cuentas;
+  mapping(string => Cuenta) cuentas;
 
-  constructor() {}
+  constructor() {
+    cuentas['Diego'].contra = '1234';
+    cuentas['Diego'].saldo = 5000;
 
-  function enviar(uint origen, uint destino, uint cantidad) public returns (bool) {
+    cuentas['Juan'].contra = '4321';
+    cuentas['Juan'].saldo = 5000;
+
+    cuentas['Daniel'].contra = '55555';
+    cuentas['Daniel'].saldo = 10;
+
+    cuentas['Universidad'].contra = 'uni';
+    cuentas['Universidad'].saldo = 1000000;
+  }
+
+  function enviar(string memory origen, string memory destino, uint cantidad) public returns (bool) {
     if (cuentas[origen].saldo >= cantidad){
       cuentas[origen].saldo -= cantidad;
       cuentas[destino].saldo += cantidad;
@@ -20,13 +32,21 @@ contract Udicoin {
     }
   }
 
-  function depositar(uint numeroCuenta, uint cantidad) public returns (bool) {
-    cuentas[numeroCuenta].saldo += cantidad;
+  function depositar(string memory cuenta, uint cantidad) public returns (bool) {
+    cuentas[cuenta].saldo += cantidad;
     return true;
   }
 
-  function getSaldo(uint numeroCuenta) public view returns (uint){
-    return cuentas[numeroCuenta].saldo;
+  function getSaldo(string memory cuenta) public view returns (uint){
+    return cuentas[cuenta].saldo;
+  }
+
+  function autenticar(string memory usuario, string memory contra) public view returns (bool){
+    if(keccak256(bytes(cuentas[usuario].contra)) == keccak256(bytes(contra)) && keccak256(bytes(contra)) != keccak256(bytes(''))){
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
--- src/app.js
+++ src/app.js
@@ -10,9 +10,13 @@ window.consultarSaldo = async (numeroCuenta) => {
     return saldo;
 };
 
-window.enviar = async (origen, destino) => {
+window.enviar = async () => {
     const accounts = await web3.eth.getAccounts();
-    const envio = await Udicoin.methods.enviar(origen, destino, 100).send({from: accounts[0]});
+    var us = sessionStorage.getItem('usuario');
+    var dest = document.getElementById('dest').value;
+    var mont = document.getElementById('mont').value;
+    console.log('us: '+us+' dest: '+dest+' mont:'+mont);
+    const envio = await Udicoin.methods.enviar(us, dest, mont).send({from: accounts[0]});
     if (!envio) {
         alert("No tienes fondos suficientes");
     }
@@ -42,7 +46,78 @@ window.onload = async () => {
         contract_udicoin.abi,
         contract_udicoin.networks[5777].address
     );
-    document.getElementById('saldo1').innerHTML = '$'+(await consultarSaldo(1))+' UDC';
-    document.getElementById('saldo2').innerHTML = '$'+(await consultarSaldo(2))+' UDC';
+    if (autenticar()){
+
+    }
 };
+
+window.autenticar = async () => {
+    const accounts = await web3.eth.getAccounts();
+    var usuario = sessionStorage.getItem('usuario');
+    var contra = sessionStorage.getItem('contra');
+    console.log('us:'+usuario+' cont:'+contra);
+    if (!await Udicoin.methods.autenticar(sessionStorage.getItem('usuario'), sessionStorage.getItem('contra')).call({from: accounts[0]})) {
+        console.log('incorrecto')
+        document.getElementById('contenido').innerHTML = `<h1>Contraseña Incorrecta</h1>`;
+        return false;
+    } else {
+        console.log('correcto');
+        document.getElementById('contenido').innerHTML = `
+        <div class="container p-3 border rounded-3">
+            <h3 class="text-primary">Bienvenido a Udico `+usuario+`</h3>
+            <h1 class="my-5"><i class="bi bi-cash-coin"></i>Saldo: $`+(await consultarSaldo(usuario))+` UDC</h1>
+            <div class="btn-group">
+                <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#transferModal">
+                    <i class="bi bi-arrow-left-right"></i>
+                    Transferir
+                </button>
+                <button type="button" class="btn btn-outline-success">
+                    <i class="bi bi-wallet2"></i>
+                    Pagar
+                </button>
+                <button type="button" class="btn btn-outline-success">
+                    <i class="bi bi-arrow-right-square"></i>
+                    Prestar
+                </button>
+            </div>
+            <button type="button" class="btn btn-outline-primary">
+                <i class="bi bi-arrow-left-square"></i>
+                Solicitar Prestamo
+            </button>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="transferModal" tabindex="-1" aria-labelledby="transferModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="transferModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="dest" class="col-form-label">Destinatario:</label>
+                        <input type="text" class="form-control" id="dest">
+                    </div>
+                    <div class="form-group">
+                        <label for="mont" class="col-form-label">Monto:</label>
+                        <input type="number" class="form-control" id="mont">
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary" onclick="enviar()">Enviar</button>
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <button class="btn btn-primary m-5" onclick="depositar('`+usuario+`')">Depositar</button>
+        `;
+        return true;
+    }
+    
+}
   
