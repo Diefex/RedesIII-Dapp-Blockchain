77c61b2f01ba9ff2e9e5438a5179de245617442b
Diefex <diefex57@gmail.com>
2021-10-07 23:19:09 -0500

    Cambios Finales

--- contracts/Udicoin.sol
+++ contracts/Udicoin.sol
@@ -2,30 +2,40 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Udicoin {
-	struct Cuenta {
+  struct Cuenta {
     string contra;
     uint saldo;
+    uint matricula;
+    uint numTx;
+    mapping(uint => string) tranx;
   }
   mapping(string => Cuenta) cuentas;
 
   constructor() {
     cuentas['Diego'].contra = '1234';
-    cuentas['Diego'].saldo = 5000;
+    cuentas['Diego'].saldo = 500000;
+    cuentas['Diego'].matricula = 200000;
 
     cuentas['Juan'].contra = '4321';
-    cuentas['Juan'].saldo = 5000;
+    cuentas['Juan'].saldo = 50000;
+    cuentas['Juan'].matricula = 50000;
 
     cuentas['Daniel'].contra = '55555';
     cuentas['Daniel'].saldo = 10;
+    cuentas['Daniel'].matricula = 10000000;
 
     cuentas['Universidad'].contra = 'uni';
     cuentas['Universidad'].saldo = 1000000;
   }
 
-  function enviar(string memory origen, string memory destino, uint cantidad) public returns (bool) {
+  function enviar(string memory origen, string memory destino, uint cantidad, string memory tranxOr, string memory tranxDes) public returns (bool) {
     if (cuentas[origen].saldo >= cantidad){
       cuentas[origen].saldo -= cantidad;
       cuentas[destino].saldo += cantidad;
+      cuentas[origen].numTx += 1;
+      cuentas[origen].tranx[cuentas[origen].numTx] = tranxOr;
+      cuentas[destino].numTx += 1;
+      cuentas[destino].tranx[cuentas[destino].numTx] = tranxDes;
       return true;
     } else {
       return false;
@@ -39,6 +49,19 @@ contract Udicoin {
 
   function getSaldo(string memory cuenta) public view returns (uint){
     return cuentas[cuenta].saldo;
+  }
+
+  function getMatricula(string memory cuenta) public view returns (uint){
+    return cuentas[cuenta].matricula;
+  }
+
+  function getTranx(string memory cuenta) public view returns (string[] memory){
+    uint nTx = cuentas[cuenta].numTx;
+    string[] memory trx = new string[](nTx+1);
+    for(uint i=1; i<=nTx; i++){
+      trx[i] = cuentas[cuenta].tranx[i];
+    }
+    return trx;
   }
 
   function autenticar(string memory usuario, string memory contra) public view returns (bool){
--- src/app.js
+++ src/app.js
@@ -13,20 +13,30 @@ window.consultarSaldo = async (usuario) => {
 window.enviar = async () => {
     var dest = document.getElementById('dest').value;
     var mont = document.getElementById('mont').value;
-    if(transferir(dest, mont)){}
+    var us = sessionStorage.getItem('usuario');
+    if(await transferir(dest, mont, 'Enviados $'+mont+' UDC a '+dest, 'recibidos $'+mont+' UDC de '+us)){}
 };
 
 window.pagarDP = async () => {
+    const accounts = await web3.eth.getAccounts();
+    var us = sessionStorage.getItem('usuario');
     for (var i = 0; i < document.DPs.radioDP.length; i++){ 
       	if (document.DPs.radioDP[i].checked) {
             var mont = document.DPs.radioDP[i].value 
          	break; 
  		}
    	}
-    if(transferir('Universidad', mont)){}
+    if(await transferir('Universidad', mont, 'Pagados $'+mont+' UDC por Derechos Pecunarios', 'Pago de Derechos Pecunarios de '+us+' por valor de $'+mont+' UDC')){}
 }
 
-window.transferir = async (dest, mont) => {
+window.pagarMT = async () => {
+    const accounts = await web3.eth.getAccounts();
+    var us = sessionStorage.getItem('usuario');
+    const mont = await Udicoin.methods.getMatricula(us).call({from: accounts[0]});
+    if(await transferir('Universidad', mont, 'Pago de matricula por $'+mont+' UDC', 'Pago de Matricula de '+us+' por valor de $'+mont+' UDC')){}
+}
+
+window.transferir = async (dest, mont, txOr, txDs) => {
     const accounts = await web3.eth.getAccounts();
     var us = sessionStorage.getItem('usuario');
     var saldo = await consultarSaldo(us);
@@ -40,8 +50,15 @@ window.transferir = async (dest, mont) => {
     alerta.innerHTML = '<div class="alert alert-primary alert-dismissible" role="alert"><div class="spinner-border text-primary" role="status"></div>Procesando transferencia<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
     document.getElementById('alertas').append(alerta);
     try{
-        if(await Udicoin.methods.enviar(us, dest, mont).send({from: accounts[0]})){
+        if(await Udicoin.methods.enviar(us, dest, mont, txOr, txDs).send({from: accounts[0]})){
             document.getElementById('saldo').innerHTML = await consultarSaldo(us);
+            var transacciones = await Udicoin.methods.getTranx(us).call({from: accounts[0]});
+            document.getElementById('transacciones').innerHTML = '';
+            for(var i=0; i<transacciones.length; i++){
+                tr = document.createElement('tr');
+                tr.innerHTML = '<td>'+transacciones[i]+'</td>';
+                document.getElementById('transacciones').append(tr);
+            }
             alerta.innerHTML = '<div class="alert alert-success alert-dismissible" role="alert">Transferencia Realizada!<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
             document.getElementById('alertas').append(alerta);
             return true;
@@ -85,6 +102,10 @@ window.onload = async () => {
     );
     if (await autenticar()){
         var usuario = sessionStorage.getItem('usuario');
+        const accounts = await web3.eth.getAccounts();
+        var matricula = await Udicoin.methods.getMatricula(usuario).call({from: accounts[0]});
+        var transacciones = await Udicoin.methods.getTranx(usuario).call({from: accounts[0]});
+        console.log(usuario=='Universidad');
         document.getElementById('contenido').innerHTML = `
         <div class="container p-3 border rounded-3">
             <h3 class="text-primary">Bienvenido a Udico `+usuario+`</h3>
@@ -94,96 +115,127 @@ window.onload = async () => {
                     <i class="bi bi-arrow-left-right"></i>
                     Enviar Saldo
                 </button>
+                `+( (usuario=='Universidad')?``:`
                 <button type="button" class="btn btn-outline-success"  data-bs-toggle="modal" data-bs-target="#payModal">
                     <i class="bi bi-wallet2"></i>
                     Pagar Derechos Pecunarios
                 </button>
-                <button type="button" class="btn btn-outline-success"  data-bs-toggle="modal" data-bs-target="#payModal">
+                <button type="button" class="btn btn-outline-success"  data-bs-toggle="modal" data-bs-target="#matModal">
                     <i class="bi bi-journal-bookmark"></i>
                     Pagar Matricula
-                </button>
+                </button>`)+`
             </div>
         </div>
 
         <div class="modal fade" id="transferModal" tabindex="-1" aria-labelledby="transferModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="transferModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <div class="form-group">
-                            <label for="dest" class="col-form-label">Destinatario:</label>
-                            <input type="text" class="form-control" id="dest">
-                        </div>
-                        <div class="form-group">
-                            <label for="mont" class="col-form-label">Monto:</label>
-                            <input type="number" class="form-control" id="mont">
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick="enviar()">Enviar</button>
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="transferModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="dest" class="col-form-label">Destinatario:</label>
+                                <input type="text" class="form-control" id="dest">
+                            </div>
+                            <div class="form-group">
+                                <label for="mont" class="col-form-label">Monto:</label>
+                                <input type="number" class="form-control" id="mont">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick="enviar()">Enviar</button>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
 
         <div class="modal fade" id="payModal" tabindex="-1" aria-labelledby="payModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="payModalLabel">Pago derechos pecuniarios</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form name=DPs>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="radioDP" id="radioDP1" value=15100>
-                        <label class="form-check-label" for="radioDP1">
-                            Pagar certificado de notas $15.100
-                        </label>
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="payModalLabel">Pago derechos pecuniarios</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="radioDP" id="radioDP2" value=7600>
-                        <label class="form-check-label" for="radioDP2">
-                            Pagar constancia de estudio $7.600
-                        </label>
+                    <div class="modal-body">
+                        <form name=DPs>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="radioDP" id="radioDP1" value=15100>
+                                <label class="form-check-label" for="radioDP1">
+                                    Pagar certificado de notas $15.100
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="radioDP" id="radioDP2" value=7600>
+                                <label class="form-check-label" for="radioDP2">
+                                    Pagar constancia de estudio $7.600
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="radioDP" id="radioDP3" value=30300>
+                                <label class="form-check-label" for="radioDP3">
+                                    Duplicado de carné $30.300
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="radioDP" id="radioDP4" value=90900>
+                                <label class="form-check-label" for="radioDP4">
+                                    Cursos vacacionales $90.900
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="radioDP" id="radioDP5" value=3000>
+                                <label class="form-check-label" for="radioDP5">
+                                    Multa de biblioteca $3.000
+                                </label>
+                            </div>
+                        </form>
                     </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="radioDP" id="radioDP3" value=30300>
-                        <label class="form-check-label" for="radioDP3">
-                            Duplicado de carné $30.300
-                        </label>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick="pagarDP()">Enviar</button>
                     </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="radioDP" id="radioDP4" value=90900>
-                        <label class="form-check-label" for="radioDP4">
-                            Cursos vacacionales $90.900
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="radioDP" id="radioDP5" value=3000>
-                        <label class="form-check-label" for="radioDP5">
-                            Multa de biblioteca $3.000
-                        </label>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick="pagarDP()">Enviar</button>
-            </div>
+                </div>
             </div>
         </div>
+
+        <div class="modal fade" id="matModal" tabindex="-1" aria-labelledby="transferModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="transferModalLabel">Pagar Matricula</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <h4>El valor de su matricula es de:</h4>
+                        <p><h2>$`+matricula+` UDC</h2></p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onclick="pagarMT()">Pagar</button>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div id="alertas"></div>
-        <button class="btn btn-primary m-5" onclick="depositar('`+usuario+`')">Depositar</button>
+        <div class="container">
+            <table class="table table-striped table-hover">
+                <thead><tr class="table-dark"><th>Transacciones</th></tr></thead>
+                <tbody id="transacciones"></tbody>
+            </table>
+        </div>
         `;
+
+        tranx = '';
+        for(var i=0; i<transacciones.length; i++){
+            tranx += '<tr><td>'+transacciones[i]+'</td></tr>';
+        }
+        document.getElementById('transacciones').innerHTML = tranx;
     }
 };
 
